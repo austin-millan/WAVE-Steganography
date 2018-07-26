6563b13623e11c69ad4b9f4234bee8d5c964553c
austin-millan <austin.millan@protonmail.com>
2018-07-26 00:50:12 -0700

    function for converting u8 arrays into a decimal value, assuming little-endian byte order. Encoder and decoder can now write/read payload length.

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,39 @@ extern crate image;
 extern crate scramble_image;
 extern crate byteorder;
 
+use std::fs;
+use std::path::Path;
+
 // use scramble_image::chaotic_maps::*;
 
 
 fn main() {
-    // Input File
-    let wav_file = String::from("examples/cover_audio.wav");
-    let secret_text = String::from("examples/secret_text.txt");
-    utils::encoder::lsb_enc(&wav_file, &"examples/stego_audio.wav".to_string(), &secret_text);
+    // Cover
+    let wav_path = String::from("examples/cover_audio.wav");
+    // Secrets
+    let secret_text_path = String::from("examples/secret_text.txt");
+    let secret_image_path = String::from("examples/secret_image_lena.png");
+    // Output
+    let secret_out_path = String::from("examples/stego_audio.wav");
+    let extracted_image_out_path = String::from("examples/extracted_image.png");
+    let extracted_text_out_path = String::from("examples/extracted_text.txt");
+    // utils::encoder::lsb_enc(&wav_file, &"examples/stego_audio.wav".to_string(), &secret_text);
 
-    // utils::encoder::iterate_over_bits(8);
+    // Setup output file for writing
+    if Path::new(&secret_out_path).exists() {
+        println!("Removing file.");
+        fs::remove_file(&secret_out_path);
+    }
+
+    println!("Encoding text...");
+    utils::encoder::lsb_enc(&wav_path, &secret_out_path, &secret_text_path);
+
+    println!("Decoding text...");
+    utils::decoder::lsb_dec(&secret_out_path, &extracted_text_out_path);
+
+    // Setup output file for writing
+    if Path::new(&secret_out_path).exists() {
+        println!("Removing file.");
+        fs::remove_file(secret_out_path);
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@
 //#![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_must_use)]
-#![allow(unused_mut)]
+//#![allow(unused_mut)]
 
 // In the canonical WAV format using RIFF specification:
 // Header: 1:40 bytes, length: 41:43, data = 44:EOF.
@@ -17,60 +17,45 @@ pub mod encoder {
     use std::vec::Vec;
     use utils::hound::*;
     use utils::hound::{WavReader, WavWriter};
-    //use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
-    //use std::thread;
+    use utils::set_bit;
+    use utils::get_bit_at;
 
     /// param in_path: Path of WAV file
     /// Stores data of file `data_path` in the LSB of every sample found in `cover_in_path`,
     /// where ach sample has type `i16`.
-    pub fn lsb_enc(cover_in_path: &String, stego_out_path: &String, data_path: &String) {
-        check_usage(cover_in_path, stego_out_path, data_path);
+    pub fn lsb_enc(wav_path: &String, stego_out_path: &String, payload_path: &String) {
+        check_usage(wav_path, stego_out_path, payload_path);
 
-        // Get WAV spec from file
-        let mut reader = WavReader::open(&cover_in_path).unwrap();
+        // IO for reading/writing wav files, samples, ....
+        let mut reader = WavReader::open(&wav_path).unwrap();
         let spec = reader.spec();
-
-        // Setup output file for writing
         let mut writer = WavWriter::create(&stego_out_path,spec).unwrap();
 
-        // Get all WAV samples (where to embed secret)
+        // Read 16-bit samples
         let mut samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
-        // println!("Number of samples (vec size): {:?}", samples.len());
+        let (data_file_metadata, cover_file_metadata) = (fs::metadata(&payload_path).unwrap(), fs::metadata(&wav_path).unwrap());
+        // Get length for payload and cover.
+        let (secret_len, cover_len) = (data_file_metadata.len() as i32, cover_file_metadata.len() as i32);
+        println!("-> Cover length: {}\n-> Secret data length: {:?} -> (as bytes): {:b}",
+                 cover_len/2, data_file_metadata.len(), data_file_metadata.len());
 
-        let data_file_metadata = fs::metadata(&data_path).unwrap();
-        let cover_file_metadata = fs::metadata(&cover_in_path).unwrap();
-        println!("Secret data length: {:?} -> (as bytes): {:b}", data_file_metadata.len(), data_file_metadata.len());
-
-        let secret_len = data_file_metadata.len() as i32;
-        let cover_len = cover_file_metadata.len() as i32;
-
-
-        println!("Cover length: {}", cover_len/2);
-
-        // Verify secret can be stored in data section.
-        // (secret_len * 16) = number of bits in data to be stored
-        // samples.len() = total number of samples available to store 1bit/sample for LSB.
-        if (secret_len * 16) as i32 > samples.len() as i32{
+        // `32+(secret_len * 8)`: 4 bytes for storing payload size, + payload data.
+        // `samples.len()`: total number of samples available to store 1bit/sample for LSB.
+        if 32+(secret_len * 8) as i32 > samples.len() as i32{
             panic!("Secret is too large for cover audio: {} bits cannot be stored {} samples.",
                    secret_len*16, samples.len());
         }
 
-//        // Can be removed
-//        if (secret_len * 16) as i32 <= samples.len() as i32{
-//            println!("# bits to hide: {}.\n# altered samples: {} (1 bit per sample).\n\
-//                      # total available samples in cover audio: {}",
-//                     secret_len * 16, secret_len * 16, samples.len());
-//        }
-
-        // Iterate over samples
-        let mut sample_num = 0;
+        // Iterate over samples, store message length
+        let mut i: i64 = 0;
         for mut sample in samples.iter_mut() {
-            writer.write_sample(*&*sample).unwrap();
-//            println!("Sample(#{}): D: {:?} B: {:b}, 16th position: {}",
-//                     sample_num, *sample, *sample, get_bit_at(*&*sample, 15));
-
-            //thread::sleep(time::Duration::from_millis(100));
-            sample_num += 1;
+            if i < 16 {
+                let bit_to_store = get_bit_at(secret_len, i as u8);
+                let bit_to_replace = get_bit_at(**&sample as i32, 0 as u8); // LSB
+                let sample = set_bit(**&sample as i32, 0, bit_to_store as u8);
+                let res = writer.write_sample(sample as i16).unwrap();
+            }
+            i += 1;
         }
         writer.finalize().unwrap();
     }
@@ -79,39 +64,6 @@ pub mod encoder {
         println!("SPECS:\nFormat: {:?}\nSample Rate: {:?}\n# Channels: {:?}\nBits per Sample: {:?}",
         spec.sample_format, spec.sample_rate, spec.channels, spec.bits_per_sample);
     }
-
-    pub fn set_bit(mut bytes: i16, pos: u8, x: u8) -> i16{
-        println!("Bytes (before): {:b}, Decimal (after): {}", bytes, bytes);
-        //format!("{:b}", x)
-        bytes &= !(1 << pos);
-        if x.eq(&1) {
-            bytes |= (1 << pos);
-        }
-        //println!("Bytes (after): {:?}", bytes);
-        println!("Bytes (after): {:b}, Decimal (after): {}", bytes, bytes);
-        bytes
-    }
-
-    /// gets the bit at position `n`. Bits are numbered from 0 (least significant) to 31 (most significant).
-    pub fn get_bit_at(bytes: i16, pos: u8) -> bool {
-        if pos < 16 { bytes & (1 << pos) != 0 }
-        else { false }
-    }
-
-    // /// NOT WORKING YET
-//    pub fn iterate_over_bits(mut bytes: i16) {
-//        println!("Bytes (before): {:b}", bytes);
-//        //let mut bitvec = BitVector::new(30)
-//        //let bv = BitVector::from_elem(10, false);
-//        let bv2 = BitVector::new(8);
-//        //let bv3 = BitVec::from_elem(10, false);
-//        //assert_eq!(bv2.len(), 8);
-//        println!("Now iterating...");
-//        for x in bv2.iter() {
-//            println!("x: {:?}", x);
-//            //assert_eq!(x, false);
-//        }
-//    }
 
     pub fn check_usage(cover_in_path: &String, stego_out_path: &String, data_path: &String) {
         let path = Path::new(&cover_in_path);
@@ -127,6 +79,74 @@ pub mod encoder {
         }
     }
 }
+
+pub mod decoder {
+    use std::fs;
+    use std::path::Path;
+    use std::ffi::OsStr;
+    use std::vec::Vec;
+    use utils::hound::*;
+    use utils::hound::{WavReader, WavWriter};
+    use std::thread;
+    use std::time;
+    use std::fmt;
+    use utils::set_bit;
+    use utils::get_bit_at;
+    use utils::to_decimal;
+    //use utils::to_decimal_arr;
+
+    pub fn lsb_dec(stego_in_path: &String, payload_out_path: &String) {
+        // IO for reading wav files, samples, ...
+        let mut reader = WavReader::open(&stego_in_path).unwrap();
+        let spec = reader.spec();
+
+        // Read 16-bit samples
+        let mut samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
+
+        // Iterate over samples, retrieve message length
+        let mut i: i32 = 0;
+        let mut len_payload = [0u8;32];
+        for mut sample in samples.iter_mut() {
+            if i < 32 {
+                let bit_i = get_bit_at(**&sample as i32, 0);
+                if bit_i { len_payload[(31-i) as usize] = 1; }
+                else { len_payload[(31-i) as usize] = 0; }
+                // thread::sleep(time::Duration::from_millis(100));
+            }
+            else{ break; }
+            i += 1;
+        }
+        let decimal_v = to_decimal(&len_payload);
+        println!("Decimal: {}", decimal_v);
+    }
+}
+
+
+pub fn set_bit(mut bytes: i32, pos: u8, x: u8) -> i32{
+    //println!("Bytes (before): {:b}, Decimal (after): {}, x: {:b}", bytes, bytes, x);
+    bytes &= !(1 << pos);
+    if x.eq(&1) {
+        bytes |= (1 << pos);
+    }
+    //println!("Bytes (after): {:b}, Decimal (after): {}", bytes, bytes);
+    bytes
+}
+
+/// gets the bit at position `n`. Bits are numbered from 0 (least significant) to 31 (most significant).
+pub fn get_bit_at(bytes: i32, pos: u8) -> bool {
+    if pos < 32 { bytes & (1 << pos) != 0 }
+    else { false }
+}
+
+/// arr should be 4 bytes
+pub fn to_decimal(arr: &[u8]) -> i32 { // @todo: make more generic
+    let mut res: i32 = 0;
+    for item in arr.iter() {
+        res = res * 2 + *item as i32;
+    }
+    res
+}
+
 
 #[cfg(test)]
 mod test_set_bit {
