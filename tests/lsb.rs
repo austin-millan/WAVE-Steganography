12cdae58a1bcf24e3ae2a4a7237c9a05188a8cd5
austin-millan <austin.millan@protonmail.com>
2018-07-30 13:14:24 -0700

    Tests for encoding/decoding length

--- tests/lsb.rs
+++ tests/lsb.rs
@@ -2,5 +2,23 @@ extern crate steganography;
 
 #[cfg(test)]
 mod test_encoder_decoder {
-    // use steganography::*;
+    use steganography::stego::lsb::*;
+    use std::fs::metadata;
+    #[test]
+    fn test_enc_length_valid(){
+        // get length
+        let length = metadata(&String::from("examples/secret_text.txt")).unwrap().len();
+        assert_eq!(enc_length(&String::from("examples/cover_audio.wav"),
+                              &String::from("examples/stego_audio.wav"),
+                              &String::from("examples/secret_text.txt")),
+                   metadata(&String::from("examples/secret_text.txt")).unwrap().len() as u32);
+    }
+
+    #[test]
+    fn test_dec_length_valid() {
+        let length = metadata(&String::from("examples/secret_text.txt")).unwrap().len();
+        assert_eq!(dec_length(&String::from("examples/stego_audio.wav"),
+                              &String::from("examples/extracted_text.txt")),
+                   metadata(&String::from("examples/secret_text.txt")).unwrap().len() as u32);
+    }
 }
